5a045d63a5c5446dcdc4afc56c0371a835868b22
rcardin <rcardin@seidor.com>
2025-06-06 13:55:10 +0200

    wip

--- controllers/talkController.js
+++ controllers/talkController.js
@@ -49,7 +49,7 @@ export async function message (req, res) {
                     {
                         key: 'username',
                         match: {
-                            value: user
+                            value: user.username
                         }
                     }
                 ]
