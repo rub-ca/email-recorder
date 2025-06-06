9b748dd25b07d7c22d585832c6b5b1e18b40312e
rcardin <rcardin@seidor.com>
2025-06-06 19:53:30 +0200

    wip

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,10 +4,10 @@ import User from '../models/User.js'
 export async function authMiddleware (req, res, next) {
     try {
         const { n8nKey } = req.body
-        if (n8nKey && n8nKey === process.env.N8N_KEY) {
-            return next()
-        }
+        if (n8nKey && n8nKey === process.env.N8N_KEY) return next()
+    } catch (err) { }
 
+    try {
         const token = req.cookies?.accessToken
 
         if (!token) {
